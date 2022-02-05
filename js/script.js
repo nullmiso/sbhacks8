d9a51c44f9c2a6ca399d325d5d27eaeecc29f610
nullmiso –– SF <andrewyu6581@gmail.com>
2022-02-05 14:09:30 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,6 +10,11 @@ inputBox.onkeyup = (e)=>{
     let userData = e.target.value; //user enetered data
     let emptyArray = [];
     if(userData){
+        icon.onclick = ()=>{ // replace with code that searches userdata.json for matching students
+            webLink = `https://www.google.com/search?q=${selectData}`;
+            linkTag.setAttribute("href", webLink);
+            linkTag.click();
+        }
         emptyArray = suggestions.filter((data)=>{
             //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
